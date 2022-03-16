559db80130b134306e4b6f516a4f5fa89986791c
Sara Asefi <90692025+Saraasef@users.noreply.github.com>
2022-03-16 18:17:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import styled from "styled-components";
+
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+
+export default function App() {
+  return (
+    <Wrapper className="App">
+      <Background />
+      <TextSection />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+`;
